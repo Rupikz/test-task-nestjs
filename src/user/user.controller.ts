9152d8a529156547b3184a77e7839db1591b82de
n.kydryavcev <n.kydryavcev@novotorica.com>
2021-06-01 00:27:10 +0300

    add user module (not finished)

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,9 +1,30 @@
-import { Controller } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { Body, Controller, Get, Put, UseGuards } from '@nestjs/common';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { UserTransportDto } from 'src/auth/dtos/user-transport.dto';
+import { JwtAuthUserGuard } from 'src/auth/guards/jwt-user.guard';
+import { ApiOk, AuthSession } from 'src/libs/common/decorators';
+import { EditUserDto } from './dtos/edit-user.dto';
+import { ProfileDto } from './dtos/profile.dto';
 import { UserService } from './user.service';
 
 @ApiTags('User')
 @Controller('users')
+@ApiBearerAuth('USER')
+@UseGuards(JwtAuthUserGuard)
 export class UserController {
   constructor(private userService: UserService) {}
+
+  @Get()
+  @ApiOk({ summary: 'Профиль' }, { type: ProfileDto })
+  async profile(@AuthSession() session: UserTransportDto): Promise<ProfileDto> {
+    const profile = await this.userService.profile(session);
+    return new ProfileDto(profile);
+  }
+
+  @Put()
+  @ApiOk({ summary: 'Редактировать профиль' }, { type: ProfileDto })
+  async edit(@AuthSession() session: UserTransportDto, @Body() data: EditUserDto): Promise<ProfileDto> {
+    await this.userService.edit(session, data);
+    return new ProfileDto(await this.userService.getMin({ id: session.userId }));
+  }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,7 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { UserTransportDto } from 'src/auth/dtos/user-transport.dto';
+import { DeletedStatus } from 'src/libs/common/constants';
+import { UserEmailOrNicknameExistException } from 'src/libs/common/exceptions';
 import { UserDto, UserEntity, UserRepository } from 'src/libs/models/user';
 import { FindConditions } from 'typeorm';
 import { Transactional } from 'typeorm-transactional-cls-hooked';
+import { EditUserDto } from './dtos/edit-user.dto';
 
 @Injectable()
 export class UserService {
@@ -14,5 +18,30 @@ export class UserService {
 
   async getMin(findData: FindConditions<UserEntity>[] | FindConditions<UserEntity>): Promise<UserEntity> {
     return await this.userRepository.findOne({ where: findData });
+  }
+
+  async profile(session: UserTransportDto): Promise<UserEntity> {
+    const queryBuilder = await this.userRepository
+      .createQueryBuilder('user')
+      .where({ id: session.userId })
+      .leftJoinAndSelect('user.tags', 'tag', 'tag.deleted = :deleted', { deleted: DeletedStatus.NOT_DELETED })
+      .getOne();
+    return queryBuilder;
+  }
+
+  @Transactional()
+  async edit(session: UserTransportDto, data: EditUserDto): Promise<void> {
+    const findData = <FindConditions<UserEntity>>{ deleted: DeletedStatus.NOT_DELETED };
+    if(data.nickname) {
+      findData.nickname = data.nickname;
+    }
+    if(data.email) {
+      findData.email = data.email;
+    }
+    const user = await this.getMin(findData);
+    if (user) {
+      throw new UserEmailOrNicknameExistException();
+    }
+    // await this.tagRepository.update({ id: params.id }, { ...data, updatedAt: new Date() });
   }
 }
